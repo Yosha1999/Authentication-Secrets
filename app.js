593a646d668b5edee920037475154eb4c32c5b63
Yosha Y <y.yosha@cleanharbors.com>
2023-05-15 18:29:48 +0530

    oauth-google

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
@@ -20,34 +22,82 @@ app.use(session({
    // cookie: { secure: true}        // for https connection
 }));
 
-// app.use(passport.initialize());  //for < v0.4.x
+app.use(passport.initialize());  
 app.use(passport.session());
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
-app.route("/")
-    .get(function(req, res){
-        res.render("home");
-    })
-    .post();
+// /*** Works with mongoose-local strategy only */
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+
+/*** Works with any strategy */
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, {
+        id: user.id,
+        username: user.username,
+      });
+    });
+  });
+  
+passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+        return cb(null, user);
+    });
+});
+/** serialise and deserialise */
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/auth/google", 
+    passport.authenticate("google", { scope: ['profile'] })
+);
+
+app.get("/auth/google/secrets",
+    passport.authenticate("google", {failureRedirect: '/login'}), function(req, res){
+        res.redirect("/secrets");
+    });
 
 app.route("/login")
     .get(function(req, res){
-        res.render("login");
+        if(req.isAuthenticated()){
+            res.redirect("/secrets")
+        } else {
+            res.render("login");
+        }
     })
     .post(function(req, res){
         
@@ -90,7 +140,7 @@ app.route("/secrets")
         if(req.isAuthenticated()){
             res.render("secrets");
         } else {
-            res.redirect("/login");
+            res.redirect("/401");
         }
     })
     .post();
@@ -105,6 +155,9 @@ app.post("/logout", function(req, res){
     });
 });
 
+app.get("/401", function(req, res){
+    res.render("401");
+});
 
 app.listen(8080, function(){
     console.log("Server running at port 8080");
@@ -130,5 +183,7 @@ app.listen(8080, function(){
  * 
  * 
  * Level 3 - Using passport, passport-local-mongoose and express-session, implemented higher level of security. Also implements cookies for managing login session
+ * 
+ * Level 4 - Using oauth (Sign in with google)
  * 
  */
